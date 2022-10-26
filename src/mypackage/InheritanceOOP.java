f6d272038b847b6de32023ccce8ebdd98fd8ff51
Roman Pysanka <saikrallyshaman@gmail.com>
2022-10-26 17:16:51 +0300

    New files

---
+++ src/mypackage/InheritanceOOP.java
@@ -0,0 +1,21 @@
+package mypackage;
+
+public class InheritanceOOP {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        cat.name = "Vasya";
+        dog.name = "Barsik";
+        cat.color = "White";
+        dog.color = "Black";
+
+        cat.walk("Backyard");
+        dog.walk("Park");
+
+        cat.feed("Milk");
+        dog.sit();
+
+        System.out.println("Cat name is: " + cat.name);
+        System.out.println("Dog name s: " + dog.name);
+    }
+}
